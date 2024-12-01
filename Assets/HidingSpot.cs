83df9f4635cf0ab643fe776cf3bf3380e2488b8f
EDK <41874384+jeffthechristian@users.noreply.github.com>
2024-12-02 00:36:54 +0200

    some sound stuff

--- Assets/HidingSpot.cs
+++ Assets/HidingSpot.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class HidingSpot : MonoBehaviour
 {
     public GameObject player;
+    public AudioClip audioClip;
+    public AudioSource audioSource;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("GhostTarget"))
         {
             player.SetActive(false);
+            audioSource.PlayOneShot(audioClip);
         }
     }
 
